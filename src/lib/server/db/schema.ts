2328fecf9d455528970878140a747ca1236e452e
Ricardo Uemura <uemuradev@MacBook-Pro-de-Ricardo.local>
2025-10-28 23:01:25 +0900

    CRUD das rooms + sidebar no layout principal

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,14 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, integer, timestamp, boolean } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: serial('id').primaryKey(),
 	age: integer('age')
+});
+
+export const rooms = pgTable('rooms', {
+	id: serial('id').primaryKey(),
+	number: integer('number').notNull(),
+	capacity: integer('capacity'),
+	status: boolean('status').default(true),
+	createdAt: timestamp('created_at').defaultNow()
 });
