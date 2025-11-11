0b34087184a1eced9c9adbce3b3f8cced9c26ae0
Eduardo Egydio Shirai <eduardoegydioshirai599@gmail.com>
2025-11-11 20:25:20 +0900

    ajuste nos courses e criacao da tabela de datas dos cursos para melhor organizacao

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,3 @@
-export * from './schema/user';
-export * from './schema/session';
-export * from './schema/account';
-export * from './schema/verification';
-export * from './schema/rooms';
+
 export * from './schema/courses';
+export * from './schema/coursesSessions';
